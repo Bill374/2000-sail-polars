d58815305b886c1622badfc69a115c131c8d5008
Bill374 <70849518+Bill374@users.noreply.github.com>
2021-02-05 19:51:44 -0500

    Create nmea.py
    Some helper functions for connecting to the NMEA 2000 bus.

---
+++ Logger/nmea.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  5 18:52:25 2021
+
+@author: wmorland
+"""
+
+import datetime
+import os
+import can
+
+
+def start_can_bus():
+    """
+    Start the the NMEA 2000 network
+
+    Returns
+    -------
+    can.BusABC
+
+    """
+
+    os.system('sudo ip link set can0 type can bitrate 100000')
+    os.system('sudo ifconfig can0 up')
+
+    return can.interface.Bus(channel='can0', bustype='socketcan_ctypes')
+
+
+def stop_can_bus():
+    """
+    Stop the NMEA 2000 network.
+
+    Returns
+    -------
+    None.
+
+    """
+    os.system('sudo ifconfig can0 down')
+
+
+def set_filters(can0):
+    """
+    Set filters on the NMEA 2000 network.
+
+    The filters ensure that only messages directly relevent to sailing
+    performance are logged.
+
+    Parameters
+    ----------
+    can0 : can.BusABC
+        The can bus object connected to the NMEA 2000 network to be filtered
+
+    Returns
+    -------
+    None.
+
+    """
+    filters = [{"can_id": 0x11, "can_mask": 0x21, "extended": False}]
+    can0.set_filters(filters)
+
+
+def get_gps_time(can0, wait=100):
+    """
+    Return the current GPS time from the NMEA network.
+
+    If no GPS time message is heard on the network after a specified number of
+    seconds an exception is thrown.
+
+    Parameters
+    ----------
+    can0 : can.BusABC
+    wait : int, optional
+        The number of seconds to wait for a GPS time message before giving up
+        and thoring an exception. The default is 100.
+
+    Returns
+    -------
+    datetime.
+
+    """
+    return datetime.datetime.today()
