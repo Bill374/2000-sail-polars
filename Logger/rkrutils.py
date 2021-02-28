f1c75f89a18aae74d0f0dbe3c115a6c855896312
Bill374 <70849518+Bill374@users.noreply.github.com>
2021-02-27 21:21:54 -0500

    #14
    Added send_to_usb function.  Still needs testing.

--- Logger/rkrutils.py
+++ Logger/rkrutils.py
@@ -10,6 +10,8 @@ import os
 import logging
 import zipfile
 import contextlib
+from pathlib import Path
+import shutil
 from google.oauth2 import service_account as ggl_acc
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
@@ -169,3 +171,63 @@ def send_to_drive_resumable(directory='.'):
     None.
 
     """
+    return None
+
+
+def send_to_usb(pi_directory='.', file_extension='.n2k'):
+    """
+    Copy files to removable USB storage.
+
+    All files from the pi directory matching the file_extension are copied to
+    the USB storage.  If the copy is successful the files are deleted from the
+    Pi.
+    The USB drive is expected to be mounted at the location specified in the
+    environment variable USBDRIVE.  If the drive is not mounted an error is
+    logged.
+
+    Parameters
+    ----------
+    pi_directory : str, optional
+        Source directory for files. The default is '.'.
+    file_extension : str, optional
+        File extension to match. The default is '.n2k'.
+
+    Returns
+    -------
+    None.
+
+    """
+    logger = logging.getLogger('rkrutils')
+
+    usb_directory = os.getenv('USBDRIVE')
+    logger.info(f'Looking for USB drive at {usb_directory}')
+    usb_path = Path(usb_directory)
+    if not usb_path.ismount():
+        logger.warning(f'USB drive not mounted at {usb_directory}')
+        # maybe we can try to mount it
+        return None
+
+    found = 0
+    failed = 0
+    logger.info('Looking for files to copy')
+    for pi_file in os.listdir(pi_directory):
+        if pi_file.endswith(file_extension):
+            found += 1
+            logger.info(f'Moving {pi_file} to USB')
+            try:
+                shutil.move(f'{pi_directory}/{pi_file}',
+                            f'{usb_directory}/{pi_file}')
+                logger.info(f'Moving {pi_file} to USB: SUCCESS')
+            except OSError:
+                failed += 1
+                logger.warning(f'Moving {pi_file} to USB: FAIL')
+
+    if not found:
+        logger.info(f'No files ending with {file_extension}')
+    else:
+        logger.info(f'Found {found} files ending with {file_extension}')
+        logger.info(f'Successfully moved {found - failed} files')
+        if failed:
+            logger.info(f'Failed to move {failed} files')
+
+    return None
